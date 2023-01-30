a31ccc6676c6df68f49558a510c4326a6966939e
Muichiro Tokito <90001568+muichiro29@users.noreply.github.com>
2023-01-30 02:05:59 -1200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const { ErineClient } = require("erine"); ///Definimos el cleint
 
+const { GatewayIntentBits } = require("discord.js"); //Definimos los intents
+
+const client = new ErineClient({
+  intents: [
+    GatewayIntentBits.Guilds, 
+     GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers
+  ], ///Definimos los intents que usa nuestro bot
+  prefix: "!", //Prefix
+});
+
+
+client.login(""); ///Token de tu bot
