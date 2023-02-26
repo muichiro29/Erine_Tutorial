0f8f5343d643b3a4973e739345e9f66df82ec722
Muichiro Tokito <90001568+muichiro29@users.noreply.github.com>
2023-02-26 19:53:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const { ErineClient } = require("erine");
+const { Database } = require("midb");
+
+// importamos nuestra clase
 
 const client = new ErineClient({
     intents: ['Guilds', 'GuildMessages', 'MessageContent'],
-    prefix: "?"
+    prefix: async function(d) {
+        let prefix = await db.get(`prefix_${d.guild.id}`);
+        return prefix || '?'
+    }
 });
 
+/////////////
+
+const db = new Database({
+    path: "database",
+    tables: ["main", "usuarios"]
+})
+db.on('ready', () => {
+    console.log('Base de datos conectado correctamente')
+})
+db.start()
+exports.db = db
+
+//////////////
 //Handler
 
 client.load_commands('./comandos')
@@ -12,4 +31,13 @@ client.load_events('./events')
 
 ////
 
-client.login("TOKEN")
+
+
+
+
+
+
+
+
+
+client.login("MTA2OTQyMDMzODM4NTAwMjYyNw.GCSnjH.bsBQppWLnP1KDIKGfxi61YnH7Jyc8367KiCzQs")
